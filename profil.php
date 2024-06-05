744cced8987ceb97e9b45c9ec300d6058eaa5308
amandasielviana <gusti.amanda@icloud.com>
2024-06-05 12:48:03 +0800

    commit pertama: masukan hasil praktikum 2024

---
+++ profil.php
@@ -0,0 +1,44 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/Amanda.jpg" alt="Amanda" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM :</strong></td>
+                        <td align="left" width="50%">2210010209</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Gusti Amanda Sielviana</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Banjarmasin, 18 Desember 2003</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0819 5399 0119</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">gusti.amanda@icloud.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="https://www.instagram.com/gustiamaanda" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/instagram.svg" width="30" alt="instagram">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
